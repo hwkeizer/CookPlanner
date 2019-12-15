dbe4eeb0ead0a8942c3ad01b995aee6340b7cc78
Henk Keizer <hwkeizer@telfort.nl>
2019-12-15 21:32:26 +0100

    Initial commit

---
+++ src/main/java/nl/cookplanner/CookplannerApplication.java
@@ -0,0 +1,13 @@
+package nl.cookplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookplannerApplication.class, args);
+	}
+
+}
